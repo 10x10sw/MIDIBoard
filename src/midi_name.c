15b13645a6ce53dc2551313933c702a99263f023
Christian E <christian@tenbyten.com>
2020-04-02 07:51:04 -0400

    proof of concept midi note controller with hold mode switch

---
+++ src/midi_name.c
@@ -0,0 +1,18 @@
+#include <usb_names.h>
+
+#define MANUFACTURER_NAME    {'1','0','x','1','0'}
+#define MANUFACTURER_NAME_LEN 5
+#define PRODUCT_NAME { 'c','E','!',' ','T','e','e','n','s','y','-','L','C'}
+#define PRODUCT_NAME_LEN 13
+
+struct usb_string_descriptor_struct usb_string_manufacturer_name = {
+        2 + MANUFACTURER_NAME_LEN * 2,
+        3,
+        MANUFACTURER_NAME
+};
+
+struct usb_string_descriptor_struct usb_string_product_name = {
+        2 + PRODUCT_NAME_LEN * 2,
+        3,
+        PRODUCT_NAME
+};
